07dcb9b3c9b87be5e498635d3094d96fceb6cc9b
Ubuntu <lohanl301@caledonian.ac.uk>
2025-03-06 16:39:45 +0000

    fixed python code

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,6 +3,8 @@
 #Your program begins with  call to main()
 #prints "Hello, World" to the terminal window.
 def main():
-	print("Hello, World")
-if__name__=="__main__":
-	main()
+    print("Hello, World")
+
+if __name__ == "__main__":
+    main()
+
