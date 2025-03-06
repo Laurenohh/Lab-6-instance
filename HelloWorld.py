270abb26ca80f88168defce0833add9d08b6d4d8
Ubuntu <lohanl301@caledonian.ac.uk>
2025-03-06 16:20:26 +0000

    added pyhton code

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+#This is a simple python program 
+
+#Your program begins with  call to main()
+#prints "Hello, World" to the terminal window.
+def main():
+	print("Hello, World")
+if__name__=="__main__":
+	main()
